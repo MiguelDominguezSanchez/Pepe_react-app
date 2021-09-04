9a85b07f24678cd3a07a0c98f148339b6b79b781
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-04 16:44:48 +0200

    Navbar Offcanvas Component

---
+++ src/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+const Hero = () => {
+	return (
+		<div>
+			<h1>Hero</h1>
+			<Navbar />
+		</div>
+	)
+}
+
+export default Hero
